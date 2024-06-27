0d282ada8c465b2ed19da151fec1faab14031b8c
HersyaDev11 <Hersyayukione@gmail.com>
2024-06-27 15:32:49 +0700

    permulaan

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap.css') }}">
+</head>
+<body>
+    
+     <h1 class="text-center mt-3">CRUD SISWA DENGAN LARAVEL</h1>
+
+     <div class="mt-3">
+        @yield('konten')
+     </div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css') }}">
+</head>
+<body>
+    
+     <h1 class="text-center mt-3">Selamat datang di aplikasi CRUD Laravel 11</h1>
+
+</body>
+</html>
